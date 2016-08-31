6b64ec73de01c472364d6d67879a3c34df985236
xeolabs <lindsay.kay@xeolabs.com>
2016-08-31 16:06:44 +0200

    Shader fix

--- helloWebGL.js
+++ helloWebGL.js
@@ -16,10 +16,8 @@ var HelloWebGL = function (canvas, gl) {
 
     var vertShaderSource = [
         "attribute vec3 aVertexPosition;",
-        "uniform mat4 uMVMatrix;",
-        "uniform mat4 uPMatrix;",
         "void main(void) {",
-        "   gl_Position = uPMatrix * uMVMatrix * vec4(aVertexPosition, 1.0);",
+        "   gl_Position = vec4(aVertexPosition, 1.0);",
         "}"
     ].join("\n");
 
